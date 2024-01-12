0ff68153f63975541f64d42d6bf6680b5af46bab
Vince Knight <vince@vknight.org>
2024-01-12 10:56:28 +0000

    Add basic docs dir and amend ci calls.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,16 +1,16 @@
 """
 Test that the version number exists and is valid.
 """
-import packaging
+import packaging.version
 
 import coopgt
 
 
 def test_version_is_str():
-    assert type(nbchkr.__version__) is str
+    assert type(coopgt.__version__) is str
 
 
 def test_version_is_valid():
     assert (
-        type(packaging.version.parse(nbchkr.__version__)) is packaging.version.Version
+        type(packaging.version.parse(coopgt.__version__)) is packaging.version.Version
     )
